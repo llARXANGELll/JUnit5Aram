6a00dffb9240a01016e6f1ed260e087def7f1acf
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-03 19:10:11 +0300

    Почти работает...

--- src/test/java/JUnit5/DataProviderTest.java
+++ src/test/java/JUnit5/DataProviderTest.java
@@ -1,15 +1,32 @@
 package JUnit5;
 
 import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 @Tag("OneTest")
 @DisplayName("Параметрезированный тест")
 public class DataProviderTest {
 
-    @DataProvider
+    public static final Logger LOG = LoggerFactory.getLogger(DataProvider.class);
 
+    @DataProvider
+    public static Object[][] dataProvider(){
+        return new Object[][] {
+                {"Nasty", 55},
+                {"Masha", 19},
+        };
+    }
+    @Test
+    @UseDataProvider("dataProvider")
+    public void tesnIn(String name, int age) {
+        String print = String.format("Имя: %s | Вззраст: %d", name, age);
+        System.out.println(print);
+    }
 
 }
