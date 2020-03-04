a1155089fb0f686e907c63e9f4ec4961f40e2574
Хачатурян Арам Альбертович <khachaturyan.Ar.Al@sberbank.ru>
2020-03-04 12:00:37 +0300

    Обновленая не рабочая версия

--- src/test/java/JUnit5/DataProviderTest.java
+++ src/test/java/JUnit5/DataProviderTest.java
@@ -1,32 +1,22 @@
 package JUnit5;
 
-import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 
-import com.tngtech.java.junit.dataprovider.DataProvider;
-import com.tngtech.java.junit.dataprovider.DataProviderRunner;
-import com.tngtech.java.junit.dataprovider.UseDataProvider;
 
 
 @Tag("OneTest")
-@RunWith(DataProviderRunner.class)
 @DisplayName("Дата драйвер")
 public class DataProviderTest {
 
 
-    @DataProvider
-    public static Object[][] sumTestData() {
-        return new Object[][]{
-                {"Nasty", 25},
-                {"Vera", 50},
-        };
+    @BeforeAll
+    static void beforeall() {
     }
 
     @Test
-    @UseDataProvider("sumTestData")
-    public void dataProviderTest(String name, int age) {
-        System.out.println(name + " " + age);
+    void befor() {
+        System.out.println("233");
     }
+
 }
